e2860897c32000397ed0fafb24b692c4709e300f
Bas Westerbaan <bas@westerbaan.name>
2023-02-04 13:35:25 +0100

    Add script to generate example keys

---
+++ example/go.mod
@@ -0,0 +1,10 @@
+module github.com/lamps-wg/kyber-certificates/example
+
+go 1.19
+
+require github.com/cloudflare/circl v1.3.2
+
+require (
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
---
+++ example/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/pem"
+	"log"
+	"os"
+
+	"github.com/cloudflare/circl/kem/schemes"
+)
+
+type subjectPublicKeyInfo struct {
+	Algorithm pkix.AlgorithmIdentifier
+	PublicKey asn1.BitString
+}
+
+type oneAsymmetricKey struct {
+	Version    int
+	Algorithm  pkix.AlgorithmIdentifier
+	PrivateKey []byte
+	Attributes []asn1.RawValue       `asn1:"tag:0,optional"`
+	PublicKey  *subjectPublicKeyInfo `asn1:"tag:1,optional"`
+}
+
+func main() {
+	scheme := schemes.ByName("Kyber768")
+	seed := make([]byte, scheme.SeedSize())
+	pk, sk := scheme.DeriveKeyPair(seed)
+
+	ppk, _ := pk.MarshalBinary()
+	psk, _ := sk.MarshalBinary()
+
+	apk := subjectPublicKeyInfo{
+		Algorithm: pkix.AlgorithmIdentifier{
+			Algorithm: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 44363, 45, 1234, 1}, // TODO
+		},
+		PublicKey: asn1.BitString{
+			BitLength: len(ppk) * 8,
+			Bytes:     ppk,
+		},
+	}
+
+	aps, err := asn1.Marshal(psk)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ask := oneAsymmetricKey{
+		Version: 0,
+		Algorithm: pkix.AlgorithmIdentifier{
+			Algorithm: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 44363, 45, 1234, 1}, // TODO
+		},
+		PrivateKey: aps,
+	}
+
+	papk, err := asn1.Marshal(apk)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pask, err := asn1.Marshal(ask)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = pem.Encode(os.Stdout, &pem.Block{
+		Type:  "KYBER768 PRIVATE KEY",
+		Bytes: pask,
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = pem.Encode(os.Stdout, &pem.Block{
+		Type:  "KYBER768 PUBLIC KEY",
+		Bytes: papk,
+	}); err != nil {
+		log.Fatal(err)
+	}
+}
